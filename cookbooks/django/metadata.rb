27f83b612df9733648024f3b4f95807fc636698d
dwelch00 <david.welch@rightscale.com>
2010-02-03 11:42:11 -0800

    added metadata

--- cookbooks/django/metadata.rb
+++ cookbooks/django/metadata.rb
@@ -1,6 +1,11 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "David Welch"
+maintainer_email "david.welch@rightscale.com"
 license          "Apache 2.0"
-description      "Installs/Configures django"
+description      "Installs/Configures Django"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+recipe "django::default", "Installs Django"
+recipe "django::install_django", "Installs Django"
+recipe "django::app_checkout", "Checks out application from git"
+recipe "django::app_start", "Starts Django app"
